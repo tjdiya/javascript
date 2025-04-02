470fa4eb3562ff6e91e4c50d127d7dd5e9bbf373
tjdiya <diya@minebrat.com>
2025-04-02 15:04:31 -0400

    prime number

---
+++ prime.js
@@ -0,0 +1,22 @@
+
+function findPrime(limit){
+    
+    for(let i=2;i<=limit;i++){
+        if(isPrime(i)){
+            console.log(i);
+        }
+    }
+    
+}
+function isPrime(item){
+    for(let i=2;i<item;i++){
+        if(item%i===0){
+            return false;
+        }
+    }
+    return true;
+
+}
+
+
+findPrime(20);
